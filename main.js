488a5f3020fae02a06d1c2e94747e35172ae5f6f
Ta2k <tombox2020@gmail.com>
2021-05-21 00:40:35 +0200

    add update and setState

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const axios = require("axios");
 const axiosCookieJarSupport = require("axios-cookiejar-support").default;
 const tough = require("tough-cookie");
 const crypto = require("crypto");
+const { extractKeys } = require("./lib/extractKeys");
 
 class Wmswebcontrol extends utils.Adapter {
     /**
@@ -36,14 +37,19 @@ class Wmswebcontrol extends utils.Adapter {
 
         // Reset the connection indicator during startup
         this.setState("info.connection", false, true);
+        this.extractKeys = extractKeys;
         this.aToken = "";
-        this.refreshToken = "";
+        this.rToken = "";
         this.userAgent = "WMS WebControl pro/1.24.0 (iPhone; iOS 12.5.1; Scale/2.00)";
         this.appUpdateInterval = null;
         this.deviceIdArray = [];
         this.localUpdateIntervals = {};
         axiosCookieJarSupport(axios);
         this.cookieJar = new tough.CookieJar();
+        this.deviceList = [];
+        if (this.config.interval < 1) {
+            this.config.interval = 1;
+        }
 
         // in this template all states changes inside the adapters namespace are subscribed
         this.subscribeStates("*");
@@ -51,15 +57,21 @@ class Wmswebcontrol extends utils.Adapter {
             .then(() => {
                 this.setState("info.connection", true, true);
                 this.log.info("Login successful");
-                // this.getDeviceList()
-                //     .then(() => {
-                //         this.appUpdateInterval = setInterval(() => {
-                //             // this.getDeviceList();
-                //         }, this.config.interval * 60 * 1000);
-                //     })
-                //     .catch(() => {
-                //         this.log.error("Get Devices failed");
-                //     });
+                this.getDeviceInfo()
+                    .then(() => {
+                        this.getDeviceList()
+                            .then(() => {
+                                this.appUpdateInterval = setInterval(() => {
+                                    this.getDeviceStatus();
+                                }, this.config.interval * 60 * 1000);
+                            })
+                            .catch(() => {
+                                this.log.error("Get device failed");
+                            });
+                    })
+                    .catch(() => {
+                        this.log.error("PostMessage failed");
+                    });
             })
             .catch(() => {
                 this.log.error("Login failed");
@@ -121,7 +133,6 @@ class Wmswebcontrol extends utils.Adapter {
                             headers: {
                                 accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                                 "accept-language": "de-de",
-
                                 "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
                             },
                         })
@@ -130,6 +141,8 @@ class Wmswebcontrol extends utils.Adapter {
                             })
                             .catch((error) => {
                                 const code = error.config.url.split("code=")[1].split("&scope")[0];
+                                this.log.debug("code: " + code);
+                                this.log.debug("code_verifier: " + code_verifier);
                                 axios({
                                     method: "post",
                                     jar: this.cookieJar,
@@ -150,12 +163,17 @@ class Wmswebcontrol extends utils.Adapter {
                                 })
                                     .then((response) => {
                                         this.aToken = response.data.access_token;
-                                        this.refreshToken = response.data.refresh_token;
+                                        this.rToken = response.data.refresh_token;
+                                        this.refreshTokenInterval = setInterval(() => {
+                                            this.refreshToken().catch(() => {});
+                                        }, 15 * 60 * 1000); // 15min
                                         resolve();
                                     })
                                     .catch((error) => {
                                         this.log.error(error.config.url);
                                         this.log.error(error);
+                                        this.log.error("code: " + code);
+                                        this.log.error("code_verifier: " + code_verifier);
                                         reject();
                                     });
                             });
@@ -168,12 +186,223 @@ class Wmswebcontrol extends utils.Adapter {
             });
         });
     }
-    getCodeChallenge() {
-        const code_verifier = this.randomString(64);
-        const base64Digest = crypto.createHash("sha256").update(code_verifier).digest("base64");
+    refreshToken() {
+        return new Promise(async (resolve, reject) => {
+            this.log.debug("refresh token");
+            axios({
+                method: "post",
+                jar: this.cookieJar,
+                withCredentials: true,
+                url: "https://auth.warema.de/v1/connect/token",
+                headers: {
+                    accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                    "content-type": "application/x-www-form-urlencoded",
+                    "accept-language": "de-de",
+                    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1",
+                },
+                data:
+                    "client_id=devicecloud_wcpmobileapp&client_secret=nosecret&code_verifier=" +
+                    this.code_verifier +
+                    "&grant_type=refresh_token&redirect_uri=wcpmobileapp%3A%2F%2Fpages%2Fredirect&refresh_token=" +
+                    this.rToken,
+            })
+                .then((response) => {
+                    this.log.debug(JSON.stringify(response.data));
+                    this.aToken = response.data.access_token;
+                    this.rToken = response.data.refresh_token;
 
+                    resolve();
+                })
+                .catch((error) => {
+                    this.log.error(error.config.url);
+                    this.log.error(error);
+                    reject();
+                });
+        });
+    }
+    getDeviceInfo() {
+        return new Promise(async (resolve, reject) => {
+            this.log.debug("get devices");
+            axios({
+                method: "get",
+                jar: this.cookieJar,
+                withCredentials: true,
+                url: "https://devicecloudservice.prod.devicecloud.warema.de/api/v1.0/devices",
+                headers: {
+                    accept: "*/*",
+                    "content-type": "application/x-www-form-urlencoded",
+                    "user-agent": this.userAgent,
+                    "accept-language": "de-DE;q=1",
+                    authorization: "Bearer " + this.aToken,
+                },
+            })
+                .then((response) => {
+                    this.log.debug(JSON.stringify(response.data));
+                    if (response.data.result) {
+                        this.extractKeys(this, "devices", response.data.result, false, "serialNumber");
+                        this.webControlId = response.data.result[0].serialNumber;
+                        this.wcType = response.data.result[0].type.toLowerCase();
+                        axios({
+                            method: "post",
+                            jar: this.cookieJar,
+                            withCredentials: true,
+                            url: "https://devicecloudservice.prod.devicecloud.warema.de/api/v1.0/communication/" + this.wcType + "/" + this.webControlId + "/postMessage/",
+                            headers: {
+                                accept: "*/*",
+                                "content-type": "application/json",
+                                "user-agent": this.userAgent,
+                                "accept-language": "de-DE;q=1",
+                                authorization: "Bearer " + this.aToken,
+                            },
+                            data: JSON.stringify({ action: "info", changeIds: [] }),
+                        })
+                            .then((response) => {
+                                this.log.debug(JSON.stringify(response.data));
+                                if (response.data.response) {
+                                    this.extractKeys(this, "devices." + this.webControlId, response.data.response);
+                                }
+                                resolve();
+                            })
+                            .catch((error) => {
+                                this.log.error(error.config.url);
+                                this.log.error(error);
+                                reject();
+                            });
+                    }
+                    resolve();
+                })
+                .catch((error) => {
+                    this.log.error(error.config.url);
+                    this.log.error(error);
+                    reject();
+                });
+        });
+    }
+    getDeviceList() {
+        return new Promise(async (resolve, reject) => {
+            this.genericPostMessage("mb8Read", {
+                address: 0,
+                block: 42,
+                eui: 1397220,
+                length: 12800,
+            })
+                .then((result) => {
+                    result = Buffer.from(result.response.data, "base64");
+                    const deviceArray = result.toString("hex").match(/(.{1,128})/g);
+                    deviceArray.forEach(async (element) => {
+                        const elementArray = element.split("ffffffffffff");
+                        const elementSerial = Buffer.from(elementArray[0].substring(0, 8), "hex").readInt32LE();
+                        const elementName = Buffer.from(elementArray[1], "hex")
+                            .toString("utf-8")
+                            .replace(/\u0000/g, "");
+                        if (elementSerial != 0) {
+                            this.deviceList.push({ id: elementSerial, name: elementName });
+                            await this.setObjectNotExistsAsync(elementName, {
+                                type: "device",
+                                common: {
+                                    name: elementSerial,
+                                    role: "indicator",
+                                    type: "mixed",
+                                    write: false,
+                                    read: true,
+                                },
+                                native: {},
+                            });
+                        }
+                    });
+                    resolve();
+                    this.log.debug(JSON.stringify(this.deviceList));
+                    this.getDeviceStatus();
+                })
+                .catch(() => {
+                    this.log.error("Get DevicesList failed");
+                    reject();
+                });
+        });
+    }
+    getDeviceStatus() {
+        return new Promise(async (resolve, reject) => {
+            this.deviceList.forEach((element) => {
+                this.genericPostMessage("manualCommandRequest", { serialNumber: element.id, functionCode: 0, setting0: 255, setting1: 255, setting2: 255, setting3: 255 })
+                    .then((result) => {
+                        this.log.debug(JSON.stringify(result.response));
+                        this.extractKeys(this, element.name, result.response, true);
+                        resolve();
+                    })
+                    .catch(() => {
+                        this.log.error("Get device status failed");
+                        reject();
+                    });
+            });
+        });
+    }
+    setDeviceStatus(device, key, value) {
+        return new Promise(async (resolve, reject) => {
+            const data = { serialNumber: device.id, functionCode: 3 };
+
+            const setting0 = await this.getStateAsync(device.name + ".setting0");
+            if (setting0) {
+                data.setting0 = setting0.val;
+            }
+            const setting1 = await this.getStateAsync(device.name + ".setting1");
+            if (setting1) {
+                data.setting1 = setting1.val;
+            }
+            const setting2 = await this.getStateAsync(device.name + ".setting2");
+            if (setting2) {
+                data.setting2 = setting2.val;
+            }
+            const setting3 = await this.getStateAsync(device.name + ".setting3");
+            if (setting3) {
+                data.setting3 = setting3.val;
+            }
+            data[key] = value;
+            this.genericPostMessage("manualCommandRequest", data)
+                .then((result) => {
+                    this.log.debug(JSON.stringify(result.response));
+                    // this.extractKeys(this, device.name, result.response, true);
+                    this.waitTimeout = setTimeout(() => {
+                        this.getDeviceStatus();
+                    }, 5000);
+                    resolve();
+                })
+                .catch(() => {
+                    this.log.error("set status failed");
+                    reject();
+                });
+        });
+    }
+    genericPostMessage(action, parameter) {
+        return new Promise(async (resolve, reject) => {
+            axios({
+                method: "post",
+                jar: this.cookieJar,
+                withCredentials: true,
+                url: "https://devicecloudservice.prod.devicecloud.warema.de/api/v1.0/communication/" + this.wcType + "/" + this.webControlId + "/postMessage/",
+                headers: {
+                    accept: "*/*",
+                    "content-type": "application/json",
+                    "user-agent": this.userAgent,
+                    "accept-language": "de-DE;q=1",
+                    authorization: "Bearer " + this.aToken,
+                },
+                data: JSON.stringify({ action: action, parameters: parameter, changeIds: [] }),
+            })
+                .then((response) => {
+                    resolve(response.data);
+                })
+                .catch((error) => {
+                    error.config.url && this.log.error(error.config.url);
+                    this.log.error(error);
+                    reject();
+                });
+        });
+    }
+    getCodeChallenge() {
+        this.code_verifier = this.randomString(64);
+        const base64Digest = crypto.createHash("sha256").update(this.code_verifier).digest("base64");
         const code_challenge = base64Digest.replace("+", "-").replace("/", "_").replace(/=+$/, "");
-        return [code_verifier, code_challenge];
+        return [this.code_verifier, code_challenge];
     }
 
     randomString(length) {
@@ -192,12 +421,9 @@ class Wmswebcontrol extends utils.Adapter {
      */
     onUnload(callback) {
         try {
-            // Here you must clear all timeouts or intervals that may still be active
-            // clearTimeout(timeout1);
-            // clearTimeout(timeout2);
-            // ...
-            // clearInterval(interval1);
-
+            clearInterval(this.appUpdateInterval);
+            clearInterval(this.refreshTokenInterval);
+            clearTimeout(this.waitTimeout);
             callback();
         } catch (e) {
             callback();
@@ -209,13 +435,18 @@ class Wmswebcontrol extends utils.Adapter {
      * @param {string} id
      * @param {ioBroker.State | null | undefined} state
      */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+    async onStateChange(id, state) {
+        if (state && !state.ack) {
+            if (id.indexOf(".setting") !== -1) {
+                const idArray = id.split(".");
+                const pre = idArray.slice(0, -1).join(".");
+                const serialNumber = await this.getStateAsync(pre + ".serialNumber");
+                this.setDeviceStatus({ id: serialNumber.val, name: idArray[2] }, idArray[idArray.length - 1], state.val).catch(() => {
+                    this.log.error("set status failed");
+                });
+            }
         } else {
             // The state was deleted
-            this.log.info(`state ${id} deleted`);
         }
     }
 
