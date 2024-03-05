a3bbd3699359a68031198a723a2a86f79de98c68
TA2k <tombox2020@gmail.com>
2024-03-05 23:23:19 +0100

    add states

--- main.js
+++ main.js
@@ -43,6 +43,120 @@ class Wmswebcontrol extends utils.Adapter {
     this.localUpdateIntervals = {};
 
     this.deviceList = [];
+    this.states = {
+      textIndexDrivingCause: {
+        498: "Heartbeat Sicherheitskontakt",
+        499: "Heartbeat Sicherheitskontakt Ende",
+        500: "Heartbeat Wind",
+        501: "Heartbeat Wind Ende",
+        502: "Heatbeat Eis",
+        503: "Heartbeat Eis Ende",
+        504: "Heartbeat Niederschlag",
+        505: "Heartbeat Niederschlag Ende",
+        506: "Windalarm",
+        507: "Windalarm Ende",
+        508: "Eisalarm",
+        509: "Eisalarm Ende",
+        510: "Niederschlag",
+        511: "Niederschlag Ende",
+        512: "Sicherheitskontakt",
+        513: "Sicherheitskontakt Ende",
+        514: "Zentralbefehl",
+        515: "Zentralbefehl Ende",
+        516: "Automatiken deaktiviert",
+        517: "Sonne",
+        518: "Wolke",
+        519: "Dämmerung morgens",
+        520: "Dämmerung abends",
+        521: "Temperatur warm",
+        522: "Temperatur kalt",
+        523: "Schaltzeitpunkt 1",
+        524: "Schaltzeitpunkt 2",
+        525: "Schaltzeitpunkt 3",
+        526: "Schaltzeitpunkt 4",
+        527: "Szene lernen",
+        528: "Manuelle Bedienung",
+        529: "Manuelle Bedienung Ende",
+        530: "Abwesend",
+        531: "Komfortposition lernen",
+        532: "Komfortposition ausführen",
+        533: "Winken",
+        534: "Laufzeit lernen starten",
+        535: "Laufzeit lernen beenden",
+        536: "System start",
+        537: "Unterbrechung einer Automatik",
+        539: "Schrittweise ausfahren",
+        540: "Anwesend",
+        541: "Thermoschutz",
+        542: "Thermoschutz Ende",
+        543: "Hindernis bei Hochfahrt",
+        544: "Hindernis bei Tieffahrt",
+        545: "Motorparametriermodus",
+        546: "Motorparametriermodus beendet",
+        547: "Akkuspannung niedrig",
+        548: "Akkuspannung niedrig Ende",
+        549: "Kommunikationsfehler mit Wendemotor",
+        550: "Positionierungsfehler",
+        unknown: "Unbekannt",
+      },
+      textIndexFunctionCode: {
+        0: " ",
+        1: " ",
+        532: "Komfortposition ausführen",
+        571: " ",
+        572: " ",
+        573: "Stop",
+        574: "Sollposition direkt",
+        575: "Sollposition",
+        576: "Impuls-wenden hoch",
+        577: "Impuls-wenden tief",
+        578: "Hochfahren",
+        579: "Tieffahren",
+        580: "Szene ausführen",
+        581: "Szene lernen",
+        582: "Toggeln",
+        583: "Aufdimmen",
+        584: "Abdimmen",
+        585: "Hochfahren Markise",
+        586: "Tieffahren Markise",
+        587: "Hochfahren Volant-Rollo",
+        588: "Tieffahren Volant-Rollo",
+        589: "Hochfahren nur Volant-Rollo 1",
+        590: "Tieffahren nur Volant-Rollo 1",
+        591: "Hochfahren nur Volant-Rollo 2",
+        592: "Tieffahren nur Volant-Rollo 2",
+        593: "Einschalten",
+        594: "Ausschalten",
+        595: "Taste Stop direkt",
+        596: "Taste Stop kurz",
+        597: "Taste Stop lang",
+        598: "Taste Stop doppelt",
+        599: "Taste Hoch direkt",
+        600: "Taste Hoch kurz",
+        601: "Taste Hoch lang",
+        602: "Taste Hoch doppelt",
+        603: "Taste Tief direkt",
+        604: "Taste Tief kurz",
+        605: "Taste Tief lang",
+        606: "Taste Tief doppelt",
+        607: "Zentralbefehl setzen",
+        608: "Blockierung durch Zentralbefehl lösen",
+        609: "Abwesend setzen",
+        610: "Abwesend löschen",
+        611: "letzten manuellen nachholen",
+        612: "letzten manuellen oder Komfortautomatikbefehl nachholen",
+        613: "Winken",
+        614: "Winken Volant-Rollo 1",
+        615: "Winken Volant-Rollo 2",
+        616: "Einen Schritt (10%) hoch",
+        617: "Einen Schritt (10%) tief",
+        618: "Letzten Befehl nachholen",
+        621: "Stop von Subnetzpartner",
+        622: "Sollposition von Subnetzpartner",
+        623: "Winkbefehl von Subnetzpartner",
+        unknown: "Unbekannt",
+      },
+    };
   }
 
   /**
@@ -437,7 +551,7 @@ class Wmswebcontrol extends utils.Adapter {
       })
         .then((result) => {
           this.log.debug(JSON.stringify(result.response));
-          this.json2iob.parse(element.name, result.response, { forceIndex: true });
+          this.json2iob.parse(element.name, result.response, { forceIndex: true, states: this.states, write: true });
         })
         .catch(() => {
           this.log.error("Get device status failed");
@@ -467,7 +581,7 @@ class Wmswebcontrol extends utils.Adapter {
     await this.genericPostMessage("manualCommandRequest", data)
       .then((result) => {
         this.log.debug("set status with result:");
-        this.log.debug(JSON.stringify(result.response));
+        result.response && this.log.debug(JSON.stringify(result.response));
         // this.extractKeys(this, device.name, result.response, true);
         this.waitTimeout = setTimeout(() => {
           this.getDeviceStatus()
